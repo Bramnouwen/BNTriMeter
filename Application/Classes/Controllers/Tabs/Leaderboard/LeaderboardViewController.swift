21688c707d7ab5afef5cb158f25251d8786535f1
Bram Nouwen <nouwenbram@gmail.com>
2017-11-27 13:40:13 +0100

    Tabs setup

---
+++ Application/Classes/Controllers/Tabs/Leaderboard/LeaderboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LeaderboardViewController.swift
+//  TriMeter
+//
+//  Created by Bram Nouwen on 27/11/17.
+//  Copyright © 2017 Bram Nouwen. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        view.applyGradient()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Application/Classes/Controllers/Tabs/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  TriMeter
+//
+//  Created by Bram Nouwen on 27/11/17.
+//  Copyright © 2017 Bram Nouwen. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 2
+        
+    }
+
+}
